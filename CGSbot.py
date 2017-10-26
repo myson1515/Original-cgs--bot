799716c0355155182e32ddd85d3c7608b91cdb9c
Ubuntu <ubuntu@ip-172-31-45-209.us-east-2.compute.internal>
2017-10-26 21:13:01 +0000

    slite change

--- CGSbot.py
+++ CGSbot.py
@@ -1,9 +1,10 @@
 import os
 import asyncio
 import discord
+from daemonize import Daemonize
 from discord.ext import commands
 bot = commands.Bot(command_prefix='prefix', description='Chatterbox')
-
+pid = "/tmp/cgsbot.pid"
 @bot.event
 async def on_message(message):
     mesString = message.content
@@ -18,7 +19,10 @@ async def on_message(message):
     channel = message.channel
     if message.content.startswith("!cgsbot"): await bot.send_message(channel, text)
 
+daemon = Daemonize(app="cgsbot", pid=pid, action=on_message)
+
+
+
 bot.run('MzYzNDY2Njg2MTMwODE0OTg3.DNEpwg.J9xWSLkaiqOerH1CiZDQgu7ktYA')
-
-
+daemon.start()
 
