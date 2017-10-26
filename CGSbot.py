eccc26efa3a7913af77e798432b5120edbdeffc2
Ubuntu <ubuntu@ip-172-31-45-209.us-east-2.compute.internal>
2017-10-26 21:30:19 +0000

    Today's Update

--- CGSbot.py
+++ CGSbot.py
@@ -18,11 +18,9 @@ async def on_message(message):
     text = fileToOpen
     channel = message.channel
     if message.content.startswith("!cgsbot"): await bot.send_message(channel, text)
-
-daemon = Daemonize(app="cgsbot", pid=pid, action=on_message)
-
-
-
+    if message.content.startswith("!help"):
+        await bot.send_message(channel,"Welcome to help!\n --------------")
+        await bot.send_message(channel, "To talk to my artificial inteligence module please type !cgsbot 'what you want to say to the bot'")
+        await bot.send_message(channel, "This bot is still in beta testing and may go offline occasionally.  This is because we are working on adding some new features to benefit you.")
+        await bot.send_message(channel, "--------------\n If there are any issues with the bot or you have suggestions to add to the bot please DM the developer at @myson1515#2928")
 bot.run('MzYzNDY2Njg2MTMwODE0OTg3.DNEpwg.J9xWSLkaiqOerH1CiZDQgu7ktYA')
-daemon.start()
-
