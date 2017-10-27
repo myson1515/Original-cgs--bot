0a864e69e17d8cc5a39029ff993a33dd6b9cb2d9
Ubuntu <ubuntu@ip-172-31-45-209.us-east-2.compute.internal>
2017-10-27 01:10:40 +0000

    Today's update

--- CGSbot.py
+++ CGSbot.py
@@ -23,4 +23,5 @@ async def on_message(message):
         await bot.send_message(channel, "To talk to my artificial inteligence module please type !cgsbot 'what you want to say to the bot'")
         await bot.send_message(channel, "This bot is still in beta testing and may go offline occasionally.  This is because we are working on adding some new features to benefit you.")
         await bot.send_message(channel, "--------------\n If there are any issues with the bot or you have suggestions to add to the bot please DM the developer at @myson1515#2928")
+
 bot.run('MzYzNDY2Njg2MTMwODE0OTg3.DNEpwg.J9xWSLkaiqOerH1CiZDQgu7ktYA')
