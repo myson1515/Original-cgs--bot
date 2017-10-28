b9c42212efc6d93a25c9118eaf942c8828858279
Ubuntu <ubuntu@ip-172-31-45-209.us-east-2.compute.internal>
2017-10-28 23:54:45 +0000

    Today's update

--- CGSbot.py
+++ CGSbot.py
@@ -4,16 +4,17 @@ import discord
 from daemonize import Daemonize
 from discord.ext import commands
 print("Bot File loaded...")
+count = 0
 def empty():
     print("Memory Loaded.")
 bot = commands.Bot(command_prefix='prefix', description='Chatterbox')
 pid = "/tmp/cgsbot.pid"
 
-#fileToWrite = open("transfer.txt", "w")
+fileToWrite = open("transfer.txt", "w")
 #startREAD = "start"
-#fileToWrite.write("Hello")
-#fileToWrite.close()
-#os.system("python2 readToFile.py")
+fileToWrite.write("hi \n1")
+fileToWrite.close()
+os.system("python2 readToFile.py")
 #fileToOpen = open("transfer.txt").read()
 #text = fileToOpen
 
@@ -21,15 +22,29 @@ pid = "/tmp/cgsbot.pid"
 @bot.event
 async def on_message(message):
     mesString = message.content
+    fileToOpen = open("transfer.txt")
+    lines = fileToOpen.readlines()
+    #os.system("python2 readToFile.py")
+#    if lines[0] == 1:
+#        fileToWrite = open("transfer.txt", "w")
+#        fileToOpen = open("transfer.txt")
+#        lines = fileToOpen.readlines()
+#        mesStringFinal = mesString[8:]
+#        fileToWrite.write(mesStringFinal)
     fileToWrite = open("transfer.txt", "w")
-    mesStringFinal = mesString[8:]
+    fileToOpen = open("transfer.txt")
+    lines = fileToOpen.readlines()
+    mesStringFinal = mesString[8:] + "\n1"
     fileToWrite.write(mesStringFinal)
+    print(mesStringFinal)
     fileToWrite.close()
     os.system("python2 readToFile.py")
-    fileToOpen = open("transfer.txt").read()
-    text = fileToOpen
+    fileToOpen2 = open("transfer.txt").read()
+    text = fileToOpen2
     channel = message.channel
-    if message.content.startswith("!cgsbot"): await bot.send_message(channel, text)
+    if message.content.startswith("!cgsbot"):
+        print(text)
+        await bot.send_message(channel, text)
     if message.content.startswith("!help"):
         await bot.send_message(channel,"Welcome to help!\n --------------")
         await bot.send_message(channel, "To talk to my artificial inteligence module please type !cgsbot 'what you want to say to the bot'")
